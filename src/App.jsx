7d74a1f5fcef437b56c1e73c38377143e012dfed
Caylin James <caylinjames.dev@gmail.com>
2024-01-31 21:56:42 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <header className="bg-slate-800">
+            <div className="max-w-8xl mx-auto">
+                <div className="py-4 lg:px-8 mx-4 lg:mx-0"></div>
+            </div>
+        </header>
+    )
+}
+
+export default App;
