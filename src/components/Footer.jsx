422e76f6b87469e2e4b5d51d214e10509be07ba1
Caylin James <caylinjames.dev@gmail.com>
2024-02-02 07:32:14 -0500

    add a basic footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer() {
+    return (
+        <footer className='bg-slate-800 py-8'>
+            <div className='mx-auto max-w-7xl text-center'>
+                <p className='text-slate-200'>
+                    The Game Database is provided by the{' '}
+                    <a className='text-blue-400' href='https://rawg.io/'>
+                        RAWG Video Games Database
+                    </a>{' '}
+                    API.
+                </p>
+            </div>
+        </footer>
+    );
+}
