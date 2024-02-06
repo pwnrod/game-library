6ab94a620fe6c4d1c4e00e9bba61d8be23c7bcf6
Caylin James <caylinjames.dev@gmail.com>
2024-02-06 08:37:26 -0500

    fix some responsive issues

--- src/components/GamesList.jsx
+++ src/components/GamesList.jsx
@@ -2,7 +2,7 @@ import Game from './Game';
 
 export default function GamesList({ games }) {
     return (
-        <ul className='grid w-full grid-cols-4 items-start gap-6 pt-8'>
+        <ul className='grid w-full grid-cols-1 items-start gap-6 pt-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
             {games.map((game) => {
                 return <Game key={game.id} game={game} />;
             })}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 export default function Hero() {
     return (
-        <section className='mx-auto flex max-w-2xl flex-col justify-center py-20 text-center text-slate-100'>
+        <section className='mx-4 flex max-w-2xl flex-col justify-center py-20 text-center text-slate-100 md:mx-auto'>
             <h1 className='pb-6 text-5xl font-bold'>Game Library</h1>
             <p>
                 Welcome to the Game Library! Explore different games, see
