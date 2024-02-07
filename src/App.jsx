a895ab5b5fdd86c2c8584b7cd6c10e439d474281
Caylin James <caylinjames.dev@gmail.com>
2024-02-06 20:43:50 -0500

    restructure app to start working on the gamesList

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
+import { useState } from 'react';
 import Header from './components/Header';
-import Hero from './components/Hero';
 import Footer from './components/Footer';
-import GamesList from './components/GamesList';
-import TagsList from './components/TagsList';
-import { games } from './mock-game-data';
-import { tags } from './mock-tags-data';
+import SearchGames from './components/SearchGames';
+import MyList from './components/MyList';
 
 function App() {
+    const [currentScreen, setCurrentScreen] = useState('search');
+    const [gamesList, setGamesList] = useState(
+        localStorage.getItem('gamesList'),
+    );
+
+    function handleToggleScreen() {
+        setCurrentScreen((curScreen) => {
+            return curScreen === 'search' ? 'myList' : 'search';
+        });
+    }
+
     return (
         <div className='mx-auto max-w-7xl'>
-            <Header />
-            <Hero />
-            <section
-                className='mx-4 flex flex-col items-center justify-center pb-20 lg:mx-0 lg:px-8'
-                aria-label='Search Area'
-            >
-                <input
-                    className='h-10 w-full max-w-2xl place-self-center rounded-full bg-slate-700
-                        px-4 ring-1 ring-slate-300/10 transition-all duration-300 hover:bg-slate-50 focus:bg-slate-50 focus:outline-none'
-                    placeholder='Search games'
-                />
-                <div className='max-w-2xl pt-4'>
-                    <TagsList tags={tags.results} />
-                </div>
-                <GamesList games={games.results} />
-            </section>
+            <Header
+                onToggleScreen={handleToggleScreen}
+                currentScreen={currentScreen}
+            />
+            {currentScreen === 'search' && <SearchGames />}
+            {currentScreen === 'myList' && <MyList />}
             <Footer />
         </div>
     );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,16 @@
-export default function Header() {
+export default function Header({ onToggleScreen, currentScreen }) {
     return (
         <header className='bg-slate-900'>
-            <div className='mx-4 grid grid-cols-3 items-center border-b border-slate-300/10 py-4 lg:mx-0 lg:px-8'>
-                <div>
-                    <span className='text-slate-200'>game.library</span>
-                </div>
+            <div className='mx-4 flex items-center justify-between border-b border-slate-300/10 py-4 text-slate-100 lg:mx-0 lg:px-8'>
+                <span>game.library</span>
+                <button
+                    className='rounded-md bg-slate-100 px-4 py-2 text-slate-800'
+                    onClick={onToggleScreen}
+                >
+                    {currentScreen === 'search'
+                        ? 'View My List'
+                        : 'Search Games'}
+                </button>
             </div>
         </header>
     );
