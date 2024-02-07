6efd6a281bb72d22b80726837db3bab1bc65adec
Caylin James <caylinjames.dev@gmail.com>
2024-02-07 07:58:06 -0500

    make the Hero component reusable

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,8 @@
-export default function Hero() {
+export default function Hero({ title, children }) {
     return (
         <section className='mx-4 flex max-w-2xl flex-col justify-center py-20 text-center text-slate-100 md:mx-auto'>
-            <h1 className='pb-6 text-5xl font-bold'>Game Library</h1>
-            <p>
-                Welcome to the Game Library! Explore different games, see
-                what&apos;s popular and check out some new releases.
-            </p>
+            <h1 className='pb-6 text-5xl font-bold'>{title}</h1>
+            {children}
         </section>
     );
 }
